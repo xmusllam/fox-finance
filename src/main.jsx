cee40ae0368c1b1efbc85c7cd56792e250966d81
xmusllam <xmusllam@gmail.com>
2026-05-25 21:27:48 +0300

    Implement Arabic to English number conversion
    Add function to convert Arabic and Persian numbers to English in input fields.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// تحويل الأرقام العربية والفارسية إلى إنجليزية في جميع حقول الإدخال
+const convertArabicToEnglish = (str) => {
+  return str
+    .replace(/[٠١٢٣٤٥٦٧٨٩]/g, (d) => '٠١٢٣٤٥٦٧٨٩'.indexOf(d))
+    .replace(/[۰۱۲۳۴۵۶۷۸۹]/g, (d) => '۰۱۲۳۴۵۶۷۸۹'.indexOf(d));
+};
+
+document.addEventListener('input', (e) => {
+  const target = e.target;
+  if (target.matches('input, textarea')) {
+    const converted = convertArabicToEnglish(target.value);
+    if (converted !== target.value) {
+      const nativeSetter = Object.getOwnPropertyDescriptor(
+        window.HTMLInputElement.prototype,
+        'value'
+      ).set;
+      nativeSetter.call(target, converted);
+      target.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  }
+}, true);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
